f5b408b494fd2c9fbab34ca96e8e459ff03ce535
Sylvain DELABARRE <sdelabarre@fr.ibm.com>
2023-08-10 14:05:35 +0200

    Update app.py v2

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World ! - Pyflask Demo</h1>'
+    return '<h1>Hello, this is a World ! - Pyflask Demo</h1>'
 
 @app.route('/version')
 def get_version():
-    return '<h1>App version : <b>1.0</b></h1>'
+    return '<h1>App version : <b>2.0</b></h1>'
 
 @app.route('/test')
 def get_test():
